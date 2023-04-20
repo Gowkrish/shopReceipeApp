b62f47a961bcc69dd8de4f6eedd1351b62f1b41b
Gowtham <gowkct@gmail.com>
2023-04-20 21:03:49 +0530

    Initial structure and recipe mock data
    #56 course

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'a test desc', '../src/assets/Nungu-pine.jpeg'),
+    new Recipe('A Test Recipe', 'a test desc', '../src/assets/Nungu-pine.jpeg')
+  ];
+   
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
